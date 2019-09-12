71e5b61e31adcacff8d72f6997e7a57add569333
blackrockfish <blackrockfish@126.com>
2019-09-12 17:39:47 +0800

    add WFbutton

---
+++ Assets/Script/WFButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class WFButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler,IPointerEnterHandler,IPointerExitHandler {
+    public int id;
+    public bool isHighLight;
+    public Action<WFButton> onPointerEnter, onPointerExit, onPointerDown, onPointerUp, onHighLight;
+    public Image image;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHighLight = true;
+        if (onPointerEnter != null)
+            onPointerEnter.Invoke(this);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHighLight = false;
+        if (onPointerExit != null)
+            onPointerExit.Invoke(this);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (onPointerDown != null)
+            onPointerDown.Invoke(this);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (onPointerUp != null)
+            onPointerUp.Invoke(this);
+    }
+	
+}
